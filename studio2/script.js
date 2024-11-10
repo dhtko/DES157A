f023107d7ac70be7a79e0a5bfc579fb2b3da9b01
dhtko <dhtko@ucdavis.edu>
2024-11-09 18:41:36 -0800

    studio2

--- studio2/script.js
+++ studio2/script.js
@@ -4,17 +4,34 @@
 
     const nextBtn = document.querySelector('#nextBtn');
     const prevBtn = document.querySelector('#previousBtn');
+    const tab = document.querySelector('#tab');
     let counter = 0;
 
     nextBtn.addEventListener('click', function(){
-        counter+=1;
-        console.log(counter);
+        counter++;
+        if (counter > 5){
+            counter = 5;
+        }
+        tabLabel(counter);
+            
     });
     prevBtn.addEventListener('click', function(event){
-        counter-=1;
+        counter--;
         if (counter < 0){
             counter = 0;
         }
-        console.log(counter);
+        tabLabel(counter);
+
     });
+
+    function tabLabel(object){
+        switch(object){
+            case 1: tab.innerHTML = '1'; break;
+            case 2: tab.innerHTML = '2'; break;
+            case 3: tab.innerHTML = '3'; break;
+            case 4: tab.innerHTML = '4'; break;
+            case 5: tab.innerHTML = 'end'; break;
+            default: tab.innerHTML = 'Opening'; break;
+        }
+    }
 })();
