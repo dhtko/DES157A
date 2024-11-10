03ba9b0be00391825e70d12f4a5bfe46440c48ff
dhtko <dhtko@ucdavis.edu>
2024-11-09 18:31:59 -0800

    studio2

--- studio2/script.js
+++ studio2/script.js
@@ -1,4 +1,20 @@
 (function(){
     'use strict';
     console.log('reading js');
+
+    const nextBtn = document.querySelector('#nextBtn');
+    const prevBtn = document.querySelector('#previousBtn');
+    let counter = 0;
+
+    nextBtn.addEventListener('click', function(){
+        counter+=1;
+        console.log(counter);
+    });
+    prevBtn.addEventListener('click', function(event){
+        counter-=1;
+        if (counter < 0){
+            counter = 0;
+        }
+        console.log(counter);
+    });
 })();
