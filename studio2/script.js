2c4d0ec7b7bcd5a08a46812e66eca2dbcfc0f2d1
dhtko <dhtko@ucdavis.edu>
2024-11-10 17:55:33 -0800

    studio2;

--- studio2/script.js
+++ studio2/script.js
@@ -7,6 +7,19 @@
     const tab = document.querySelector('#tab');
 
     const opening = document.querySelector('#opening');
+    
+    const sergant = document.querySelector('#sergant');
+    const outer_sgt_one = document.querySelector('#outer_sgt_one');
+    const outer_sgt_two = document.querySelector('#outer_sgt_two');
+    const outer_sgt_three = document.querySelector('#outer_sgt_three');
+    const outer_sgt_four = document.querySelector('#outer_sgt_four');
+    const outer_sgt_five = document.querySelector('#outer_sgt_five');
+    const sgt_one = document.querySelector('#sgt_one');
+    const sgt_two = document.querySelector('#sgt_two');
+    const sgt_three = document.querySelector('#sgt_three');
+    const sgt_four = document.querySelector('#sgt_four');
+    const sgt_five = document.querySelector('#sgt_five');
+    
     const closing = document.querySelector('#closing');
 
     let counter = 0;
@@ -17,7 +30,11 @@
             counter = 5;
         }
 
-        toggle(counter);
+        toggle(counter); //label and on/off
+
+        switch(counter){
+            case 4: content_four(); break;
+        }
     });
     prevBtn.addEventListener('click', function(){
         counter--;
@@ -25,7 +42,7 @@
             counter = 0;
         }
 
-        toggle(counter);
+        toggle(counter); //label and on/off
     });
 
     function toggle(object){
@@ -40,18 +57,65 @@
                 tab.innerHTML = '3'; 
                 break;
             case 4: 
-                tab.innerHTML = 'Sergant'; 
+                tab.innerHTML = 'Sergant';
+                opening.className = 'content hidden';
+                sergant.className = 'content showing';
+                closing.className = 'content hidden';
                 break;
             case 5: 
                 tab.innerHTML = 'Ending'; 
-                opening.className = 'content hidden'
+                opening.className = 'content hidden';
+                sergant.className = 'content hidden';
                 closing.className = 'content showing';
                 break;
             default: 
                 tab.innerHTML = 'Opening';
                 opening.className = 'content showing';
+                sergant.className = 'content hidden';
                 closing.className = 'content hidden';
                 break;
         }
     }
+
+    function content_four(){
+        outer_sgt_one.addEventListener('mouseover', function(){
+            sgt_one.className = 'snapshot showing';
+        });
+        outer_sgt_one.addEventListener('mouseout', function(){
+            sgt_one.className = 'snapshot hidden';
+        })
+
+        outer_sgt_two.addEventListener('mouseover', function(){
+            sgt_two.className = 'snapshot showing';
+        });
+        outer_sgt_two.addEventListener('mouseout', function(){
+            sgt_two.className = 'snapshot hidden';
+        })
+
+        outer_sgt_three.addEventListener('mouseover', function(){
+            sgt_three.className = 'snapshot showing';
+        });
+        outer_sgt_three.addEventListener('mouseout', function(){
+            sgt_three.className = 'snapshot hidden';
+        })
+
+        outer_sgt_four.addEventListener('mouseover', function(){
+            sgt_four.className = 'snapshot showing';
+        });
+        outer_sgt_four.addEventListener('mouseout', function(){
+            sgt_four.className = 'snapshot hidden';
+        })
+
+        outer_sgt_five.addEventListener('mouseover', function(){
+            sgt_five.className = 'snapshot showing';
+        });
+        outer_sgt_five.addEventListener('mouseout', function(){
+            sgt_five.className = 'snapshot hidden';
+        })
+
+
+        
+    }
+
+
 })();
