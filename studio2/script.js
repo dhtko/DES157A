56af69d5bdbcc389f6eeded3b4656b8dab311970
dhtko <dhtko@ucdavis.edu>
2024-11-10 18:42:20 -0800

    studio2

--- studio2/script.js
+++ studio2/script.js
@@ -13,12 +13,18 @@
     const outer_sgt_two = document.querySelector('#outer_sgt_two');
     const outer_sgt_three = document.querySelector('#outer_sgt_three');
     const outer_sgt_four = document.querySelector('#outer_sgt_four');
-    const outer_sgt_five = document.querySelector('#outer_sgt_five');
     const sgt_one = document.querySelector('#sgt_one');
     const sgt_two = document.querySelector('#sgt_two');
     const sgt_three = document.querySelector('#sgt_three');
     const sgt_four = document.querySelector('#sgt_four');
-    const sgt_five = document.querySelector('#sgt_five');
+    const sgt_over_one = document.querySelector('#sgt_overlay1');
+    const sgt_over_two = document.querySelector('#sgt_overlay2');
+    const sgt_over_three = document.querySelector('#sgt_overlay3');
+    const sgt_over_four = document.querySelector('#sgt_overlay4');
+    const sgt_btn_one = document.querySelector('#sgt_overlay1_btn');
+    const sgt_btn_two = document.querySelector('#sgt_overlay2_btn');
+    const sgt_btn_three = document.querySelector('#sgt_overlay3_btn');
+    const sgt_btn_four = document.querySelector('#sgt_overlay4_btn');
     
     const closing = document.querySelector('#closing');
 
@@ -83,38 +89,52 @@
         });
         outer_sgt_one.addEventListener('mouseout', function(){
             sgt_one.className = 'snapshot hidden';
-        })
+        });
+        outer_sgt_one.addEventListener('click', function(){
+            sgt_over_one.className = 'overlay showing';
+        });
+        sgt_btn_one.addEventListener('click', function(){
+            sgt_over_one.className = 'overlay hidden';
+        });
 
         outer_sgt_two.addEventListener('mouseover', function(){
             sgt_two.className = 'snapshot showing';
         });
         outer_sgt_two.addEventListener('mouseout', function(){
             sgt_two.className = 'snapshot hidden';
-        })
+        });
+        outer_sgt_two.addEventListener('click', function(){
+            sgt_over_two.className = 'overlay showing';
+        });
+        sgt_btn_two.addEventListener('click', function(){
+            sgt_over_two.className = 'overlay hidden';
+        });
 
         outer_sgt_three.addEventListener('mouseover', function(){
             sgt_three.className = 'snapshot showing';
         });
         outer_sgt_three.addEventListener('mouseout', function(){
             sgt_three.className = 'snapshot hidden';
-        })
+        });
+        outer_sgt_three.addEventListener('click', function(){
+            sgt_over_three.className = 'overlay showing';
+        });
+        sgt_btn_three.addEventListener('click', function(){
+            sgt_over_three.className = 'overlay hidden';
+        });
 
         outer_sgt_four.addEventListener('mouseover', function(){
             sgt_four.className = 'snapshot showing';
         });
         outer_sgt_four.addEventListener('mouseout', function(){
             sgt_four.className = 'snapshot hidden';
-        })
-
-        outer_sgt_five.addEventListener('mouseover', function(){
-            sgt_five.className = 'snapshot showing';
         });
-        outer_sgt_five.addEventListener('mouseout', function(){
-            sgt_five.className = 'snapshot hidden';
-        })
-
-
-        
+        outer_sgt_four.addEventListener('click', function(){
+            sgt_over_four.className = 'overlay showing';
+        });
+        sgt_btn_four.addEventListener('click', function(){
+            sgt_over_four.className = 'overlay hidden';
+        });
     }
 
 
