ad7b4d40ffaaea180ae11866424535f54063c194
dhtko <dhtko@ucdavis.edu>
2024-11-23 02:02:24 -0800

    studio3

--- studio3/script.js
+++ studio3/script.js
@@ -1,6 +1,95 @@
 (function(){
-    'using strict';
+    'use strict';
     console.log('reading js');
+
+    const instruction = document.querySelector('#instruction');
+    const overlay = document.querySelector('#overlay');
+    const close = document.querySelector('#close');
+
+    const timer = document.querySelector('#timer');
+    let count = 1;
+
+    const strikeZone = [];
+
+    const windup = document.querySelector('#throw');
+    const strike = document.querySelector('#hiddenNumber');
+
+
+    //set up the answer (from star-trek-game)
+    function setStrikeZone(){
+        const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        
+        mixDigits(digits);
+
+        for (let i = 0; i < 4; i++){
+            strikeZone.push(digits.shift());
+        }
+    }
+    function mixDigits(list){
+        for (let i = list.length - 1; i > 0; i--){
+            const j = Math.floor(Math.random() * (i + 1));
+            [list[i], list[j]] = [list[j], list[i]];
+        }
+    }
+    setStrikeZone();
+    console.log(strikeZone);
+    
+    //answer for display only
+    const strikeZoneNum = strikeZone.map(Number);
+
+
+
+
+    //opening, closing instruction
+    instruction.addEventListener('click', function(){
+        overlay.className = 'showing';
+    });
+    close.addEventListener('click', function(){
+        overlay.className = 'hidden';
+    });
+
+
+
+    //throw the ball
+    windup.addEventListener('click', function(event){
+        event.preventDefault();
+
+        //check any errors in user input
+        const pitching = document.querySelector('#pitchingZone').value;
+        if (isNaN(pitching)){ //if user entered string, warn without increasing the count
+            alert('You must type in four-digit NUMBERS to throw the ball');
+            document.querySelector('#pitchingZone').focus();
+            count -= 1;
+        }
+        else if (pitching.length > 0 && pitching.length < 4){ //if user entered a 1 to 3 digits number , warn without increasing the count
+            alert('You must type in FOUR-DIGIT numbers to throw the ball');
+            document.querySelector('#pitchingZone').focus();
+            count -= 1;
+        }
+        else if (pitching.length > 4){ //if user entered a number more than 4 digits, warn without increasing the count
+            alert('You must type in FOUR-DIGIT numbers to throw the ball');
+            document.querySelector('#pitchingZone').focus();
+            count -= 1;
+        }
+        else if (pitching == ''){
+            alert('You MUST TYPE in four-digit numbers to throw the ball');
+            document.querySelector('#pitchingZone').focus();
+            count -= 1;
+        }
+        
+        //inning scoreboard
+        if (count > 9){
+            strike.innerHTML = strikeZoneNum;
+            alert('You Lost!');
+        }
+        else{
+            count += 1;
+            timer.innerHTML = count;
+        }
+        
+    });
+
+
 
     
 })();
